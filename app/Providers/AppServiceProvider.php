a6063d590a3fe3a34fa749f596bea4efa73560a9
Henrique <hassis.moreira177@hotmail.com>
2026-06-18 16:18:43 -0300

    fix: tentando arrumar a geração de relatório

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,5 +18,7 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
     }
 }
